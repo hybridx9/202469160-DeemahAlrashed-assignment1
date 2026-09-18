08c847e285cf8c4aa8e8df814e4365fdf80865cf
hybridx9 <s202469160@kfupm.edu.sa>
2026-09-19 00:15:11 +0300

    Adding time based greeting feature

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const greeting = document.getElementById("greeting");
+const hour = new Date().getHours();
+
+if (hour < 12) {
+    greeting.textContent = "Good morning!";
+} else if (hour < 18) {
+    greeting.textContent = "Good afternoon!";
+} else {
+    greeting.textContent = "Good evening!";
+}
