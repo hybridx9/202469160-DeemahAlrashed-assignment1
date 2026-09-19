275d4eee4c3ac21fcf94bd53a7e74ad4f4dace61
hybridx9 <s202469160@kfupm.edu.sa>
2026-09-19 03:39:14 +0300

    Adding code comments

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
-const greeting = document.getElementById("greeting");
-const hour = new Date().getHours();
+//Display a greeting based on the current time
 
+const greeting = document.getElementById("greeting");
+const hour = new Date().getHours(); //Get the current hour
+
+//Change greeting based on time
 if (hour < 12) {
     greeting.textContent = "Good morning!";
 } else if (hour < 18) {
